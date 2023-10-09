09f51afec7eb9a871651c6a6a33acda3b47071fd
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-09 14:48:29 -0300

    :sparkles: feat: method to show all content

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -1,3 +1,11 @@
-class MoviesController {}
+const MoviesRepository = require('../repositories/MoviesRepository');
+
+class MoviesController {
+  async show(request, reply) {
+    const allMovies = await MoviesRepository.index();
+
+    reply.send(allMovies);
+  }
+}
 
 module.exports = new MoviesController();
