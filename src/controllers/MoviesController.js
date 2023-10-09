4aaf4f255768066227ef465789cffc8f17e01cfc
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-09 15:48:48 -0300

    :sparkles: feat: method and validation findByID

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -6,6 +6,18 @@ class MoviesController {
 
     reply.send(allMovies);
   }
+
+  async showByID(request, reply) {
+    const { id } = request.params;
+
+    const movie = await MoviesRepository.findByID(id);
+
+    if (!movie) {
+      return reply.code(404).send({ error: 'Movie or TV show not found!' });
+    }
+
+    return reply.code(200).send(movie);
+  }
 }
 
 module.exports = new MoviesController();
