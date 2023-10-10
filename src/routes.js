75c7582825dc01f92a14742c2d84a2f7e73c35d6
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-10 16:00:34 -0300

    :sparkles: feat: route PUT to update content

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ function routes(server, options, done) {
   server.get('/movies', MoviesController.show);
   server.get('/movies/:id', MoviesController.showByID);
   server.post('/movies', MoviesController.store);
+  server.put('/movies/:id', MoviesController.update);
 
   done();
 }
