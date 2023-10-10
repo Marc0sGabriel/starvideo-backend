b05555ae2b8adf659c46034a953354d0a62853a8
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-10 14:43:55 -0300

    :sparkles: feat: route POST to create content

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const MoviesController = require('./controllers/MoviesController');
 function routes(server, options, done) {
   server.get('/movies', MoviesController.show);
   server.get('/movies/:id', MoviesController.showByID);
+  server.post('/movies', MoviesController.store);
 
   done();
 }
