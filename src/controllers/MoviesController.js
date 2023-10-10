538c5776ba325d26ecbf3249841786391ee104f6
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-10 16:01:32 -0300

    :sparkles: feat: controller to update content

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -54,6 +54,28 @@ class MoviesController {
 
     return reply.code(201).send();
   }
+
+  async update(request, reply) {
+    const { title, category, genre, videoID, imageCover } = request.body;
+    const { id } = request.params;
+
+    const isAlreadyExistsMovie = MoviesRepository.findByTitle(title);
+
+    if (isAlreadyExistsMovie) {
+      return reply.code(400).send({ error: 'Content already exists!' });
+    }
+
+    await MoviesRepository.update({
+      id,
+      title,
+      category,
+      genre,
+      videoID,
+      imageCover,
+    });
+
+    return reply.code(200).send();
+  }
 }
 
 module.exports = new MoviesController();
