6caae8c9624a0af1e35396bf690d4dfa99c64a6f
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-10 14:43:10 -0300

    :sparkles: feat: validations and storing content

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -1,4 +1,5 @@
 const MoviesRepository = require('../repositories/MoviesRepository');
+const { randomUUID } = require('node:crypto');
 
 class MoviesController {
   async show(request, reply) {
@@ -17,6 +18,41 @@ class MoviesController {
     }
 
     return reply.code(200).send(movie);
+  }
+
+  async store(request, reply) {
+    const { title, category, genre, videoID, imageCover } = request.body;
+    const id = randomUUID();
+
+    const isAlreadyExistsMovie = MoviesRepository.findByTitle(title);
+    const requiredFields = [
+      'title',
+      'category',
+      'genre',
+      'videoID',
+      'imageCover',
+    ];
+
+    const missingFields = requiredFields.find((field) => !request.body[field]);
+
+    if (missingFields) {
+      reply.code(400).send({ error: `${missingFields} is required!` });
+    }
+
+    if (isAlreadyExistsMovie) {
+      reply.code(400).send({ error: 'Movie or TV Show already exists!' });
+    }
+
+    await MoviesRepository.create({
+      id,
+      title,
+      category,
+      genre,
+      videoID,
+      imageCover,
+    });
+
+    return reply.code(201).send();
   }
 }
 
