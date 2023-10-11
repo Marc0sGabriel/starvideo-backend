b7f6427c13da0dbdefbd07072dbc04e52394acc8
Marc0sGabriel <mgabriel4747@gmail.com>
2023-10-11 17:34:53 -0300

    :sparkles: feat: method to update content by id

--- src/repositories/MoviesRepository.js
+++ src/repositories/MoviesRepository.js
@@ -1,4 +1,5 @@
 const movies = require('../fake-data/movies');
+const db = require('../database');
 
 class MovieRepository {
   index() {
@@ -26,16 +27,34 @@ class MovieRepository {
     return newContent;
   }
 
-  update({ id, title, category, genre, videoID, imageCover }) {
-    const movie = movies.find((data) => data.id === id);
-    Object.assign(movie, {
-      id,
-      title,
-      category,
-      genre,
-      videoID,
-      imageCover,
-    });
+  update({
+    id,
+    title,
+    category,
+    genre,
+    videoID,
+    imageCover,
+    publishedAt,
+    seasons,
+  }) {
+    const movie = db.query(
+      `
+    UPDATE movies
+    SET
+    title = $2,
+    category = $3,
+    genre = $4,
+    videoID = $5,
+    imageCover = $6,
+    publishedAt = $7,
+    seasons = $8
+    WHERE id = $1
+    RETURNING *
+    `,
+      [id, title, category, genre, videoID, imageCover, publishedAt, seasons]
+    );
+
+    return movie;
   }
 
   delete({ id }) {
